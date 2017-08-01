38058ab26fb0abbcbdfaa1443836f67302a9d6a0
Kai Kuehner <kvk@pobox.com>
2017-07-31 20:44:59 -0400

    Improve name generation

--- Names.py
+++ Names.py
@@ -7,7 +7,10 @@ class NameGenerator(object):
         json_file = open(url, "r")
         data = json.load(json_file)
         self.consonants = data["consonants"]
+        self.start_consonants = data["start_consonants"]
+        self.end_consonants = data["end_consonants"]
         self.vowels = data["vowels"]
+        self.titles = data["titles"]
         
     def generate_name(self):
         #pick number of syllables- 1/6 chance of 1, 1/2 chance of 2, 1/3 chance of 3
@@ -22,7 +25,9 @@ class NameGenerator(object):
             if len(name) == num_syllables == 1:
                 name += self.generate_syllable()
         
-        #TODO titles ("the X")
+        if random.randrange(3) == 0:
+            name += " the " + self.random_title()
+        
         
         #capitalize first letter
         name = name[0].capitalize() + name[1:]
@@ -32,14 +37,24 @@ class NameGenerator(object):
     def generate_syllable(self):
         syllable = ""
         if random.randrange(4) > 0:
-            syllable += self.random_consonant()
+            syllable += self.random_consonant(False)
         syllable += self.random_vowel()
         if random.randrange(4) > 0:
-            syllable += self.random_consonant()
+            syllable += self.random_consonant(True)
         return syllable
     
-    def random_consonant(self):
-        return self.consonants[random.randrange(len(self.consonants))]
+    def random_consonant(self, end):
+        if end:
+            other_consonants = self.end_consonants
+        else:
+            other_consonants = self.start_consonants
+        
+        num_consonants = len(self.consonants)
+        choice = random.randrange(num_consonants + len(other_consonants))
+        return self.consonants[choice] if choice < num_consonants else other_consonants[choice - num_consonants]
     
     def random_vowel(self):
         return self.vowels[random.randrange(len(self.vowels))]
+    
+    def random_title(self):
+        return self.titles[random.randrange(len(self.titles))]
